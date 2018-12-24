fb6ca62809d1a4c72055c1dce6e39635d346dd07
Jeremy Geels <a@jgw.tf>
2018-12-24 01:11:02 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# plover_phrasing
-My phrasing system for plover, using python dictionaries
+# plover phrasing
+This is my phrasing system for plover, using python dictionaries.
+
+This system is based on frequency data gathered from datamuse and also from experience.
+
+The basic premise of this is writing sentences like "I didn't know that" or "you don't really care about" in one stroke, by defining starting left bank stroke words, middle vowel words, and ending words.
+
+Here's a quick overview:  
+**Starting words**  
+- `SWR`: `I`
+- `KPWR`: `you`
+- `KWHR`: `he`
+- `SKWHR`: `she`
+- `TWH`: `they`
+- `TWR`: `we`
+- `TKWH`: `it`
+
+**Middle words**  
+- `OE`: `don't` (`AOE` makes `really don't`, `OEU` makes `don't really`)
+- `AU`: `didn't` (`AOU` makes `really didn't`, `AEU` makes `didn't really`)
+- `E`: `doesn't` (`AE` makes `really doesn't`, `EU` makes `doesn't really`)
+- `O`: `can't` (`AO` makes `really can't`, `OU` makes `can't really`)
+- `A` or `U`: `really`
+- `AOEU`: `don't even`
+
+**Ending words** (non-exhaustive)  
+- `PB`: `know`
+- `P`: `want`
+- `RPL`: `remember`
+- `BL`: `believe`
+- `FG`: `forget`
+- `R`: `are`, or `am`, or `is` depending on starting word
+- et cetera
+
+I will also soon merge this with a similar system that uses the same ending words.
---
+++ ps2.py
@@ -0,0 +1,154 @@
+LONGEST_KEY = 1
+
+starters = {	"SWR":"I",
+		"KPWR":"you",
+		"KWHR":"he",
+		"SKWHR":"she",
+		"TWH":"they",
+		"TWR":"we",
+		"TKWH":"it",
+		"PWH":"",
+		"SPWH":""}
+
+__are = {	"SWR":"am",
+		"KPWR":"are",
+		"KWHR":"is",
+		"SKWHR":"is",
+		"TWH":"are",
+		"TWR":"are",
+		"TKWH":"is",
+		"PWH":"are",
+		"SPWH":"is"}
+
+
+middles = { 	"A":"really",
+		"U":"really",
+		"O":"can't",
+		"AO":"really can't",
+		"OU":"can't really",
+		"OE":"don't",
+		"AOE":"really don't",
+		"OEU":"don't really",
+		"E":"doesn't",
+		"AE":"really doesn't",
+		"EU":"doesn't really",
+		"AU":"didn't",
+		"AEU":"didn't really",
+		"AOU":"really didn't",
+		"AOEU":"don't even",
+		"":""}
+
+ends = {	"PB":"know",
+		"PBT":"know that",
+		"PBTS":"knows that",
+		"*PBT":"know the",
+		"*PBTS":"knows the",
+		"P":"want",
+		"PT":"want to",
+		"PTS":"wants to",
+		"*PT":"want the",
+		"*PTS":"wants the",
+		"*P":"wanna",
+		"RPL":"remember",
+		"RPLT":"remember that",
+		"RPLTS":"remembers that",
+		"*RPLT":"remember the",
+		"*RPLTS":"remembers the",
+		"BG":"can",
+                "BGT":"can't",
+		"BGD":"could",
+		"BL":"believe",
+		"D":"had",
+		"F":"have",
+		"FZ":"has",
+		"FS":"was",
+		"FT":"have to",
+		"FTS":"has to",
+		"*FT":"have the",
+		"*FTS":"has the",
+		"FPLT":"must",
+		"L":"will",
+		"LD":"would",
+		"PBG":"think",
+		"PBGT":"think that",
+		"PBGTS":"thinks that",
+		"*PBGT":"think the",
+		"*PBGTS":"thinks the",
+		"PL":"may",
+		"PLT":"might",
+		#"R":"are", # special case this one
+		#"RT":"are not" # also special case this
+		"RB":"shall",
+		"RBD":"should",
+		"RL":"recall",
+		"RP":"were",
+		"RPBD":"understand",
+		"*RPBD":"understood",
+		"PBD":"need",
+                "PBTD":"need to",
+                "PBTSD":"needs to",
+                "*PBTD":"need the",
+                "*PBTSD":"needs the",
+		"FL":"feel",
+                "FLG":"feel like",
+		"FLT":"felt",
+                "FLGT":"felt like",
+		"PBL":"mean",
+		"PBLT":"meant",
+		"BLG":"like",
+                "BLGT":"like to",
+                "BLGTS":"likes to",
+                "*BLGT":"like the",
+                "*BLGTS":"likes the",
+		"LG":"love",
+		"LGT":"love to",
+		"LGTS":"loves to",
+		"*LGT":"love the",
+		"*LGT":"loves the",
+		"RBG":"care",
+		"RBGT":"care about",
+		"RBGTS":"cares about",
+		"GT":"get",
+		"*GT":"got",
+                "PLD":"mind",
+                "FG":"forget",
+                "FGT":"forgot",
+                "FRB":"wish",
+                "PGT":"expect",
+                "FPB":"even",
+                "PBLG":"just",
+		"":""}
+
+def lookup(key):
+	assert len(key) <= LONGEST_KEY
+
+	ks = ""
+	km = ""
+	ke = "*" if ("*" in key[0]) else ""
+	c = 0
+	for i in key[0]:
+		if c == 0:
+			if i in "AOEU-*": c+=1
+			else: ks += i
+		if c == 1:
+			if not i in "AOEU-*": c+=1
+			elif i in "AOEU": km += i
+		if c == 2:
+			ke += i
+
+	if not ks in starters: raise KeyError
+	if not km in middles: raise KeyError
+
+	# -R special casing
+	if ke != "R" and ke != "RT":
+		if not ke in ends: raise KeyError
+		ret = starters[ks] + " " + middles[km] + " " + ends[ke]
+	else:
+		if ke == "R":
+			ret = starters[ks] + " " + middles[km] + " " + __are[ks]
+		else:
+			ret = starters[ks] + " " + middles[km] + " " + __are[ks] + " not"
+
+	if ret == "  ": raise KeyError
+
+	return " ".join(ret.split())
